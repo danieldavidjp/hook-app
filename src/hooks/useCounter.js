27471b9e83dbcb6cd6711218384c43701f5fed44
daniel <you@example.com>
2024-03-18 22:36:11 +0100

    Custom hook implementation

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -11,7 +11,7 @@ export const useCounter = ( initialValue = 10) => {
    const decrement = (value = 1) => {
       if (counter <= 0) return;
    
-      setCounter(counter - 1);
+      setCounter(counter - value);
    }
    const reset = () => {
       setCounter(initialValue);
